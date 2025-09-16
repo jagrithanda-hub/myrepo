99eab452ed1c34169d0df2d79800e5ac84b3f8e7
Jagrithanda <jagrithanda206@gmail.com>
2025-09-16 15:33:14 +0530

    new question

--- practicequestion.js
+++ practicequestion.js
@@ -1,92 +1,104 @@
-//question-1
-console.log("enter a string to check if its palindrome or not: ");
-process.stdin.on("data",function(inputfromuser){
-    let string=inputfromuser.toString().trim();
-    let reverse = ""
-    for(let i=string.length-1;i>=0;i--){
-        reverse+=string[i];
+// //question-1
+// console.log("enter a string to check if its palindrome or not: ");
+// process.stdin.on("data",function(inputfromuser){
+//     let string=inputfromuser.toString().trim();
+//     let reverse = ""
+//     for(let i=string.length-1;i>=0;i--){
+//         reverse+=string[i];
         
 
-    };
-    if(string.toLowerCase()==reverse.toLowerCase()){
-            console.log("true");
-    }else{
-            console.log("false");
-    };
+//     };
+//     if(string.toLowerCase()==reverse.toLowerCase()){
+//             console.log("true");
+//     }else{
+//             console.log("false");
+//     };
     
-    process.exit();
-});
-//question-2
-console.log("enter the number : ");
-process.stdin.on("data",function(inputfromuser){
-    let num = Number(inputfromuser);
-    let result=1
-    for (let i =1;i<=num;i++){
-            result*=i
+//     process.exit();
+// });
+// //question-2
+// console.log("enter the number : ");
+// process.stdin.on("data",function(inputfromuser){
+//     let num = Number(inputfromuser);
+//     let result=1
+//     for (let i =1;i<=num;i++){
+//             result*=i
             
         
-    }console.log(result);
-    process.exit();
+//     }console.log(result);
+//     process.exit();
     
-});
-//question-3
-console.log("enter the number : ");
-process.stdin.on("data",function(inputfromuser){
-    let num = inputfromuser.toString().trim()
-    let result= ""
-    for(let i=num.length-1;i>=0;i--){
-        result+=num[i];
-    }console.log(result);
-    process.exit()
-});
-// question-4
-let arr=[1,2,3,4,5,6,7,8,9,1000,56789];
-console.log(Math.max(...arr));
-console.log(Math.min(...arr));
-//quetsion-5
+// });
+// //question-3
+// console.log("enter the number : ");
+// process.stdin.on("data",function(inputfromuser){
+//     let num = inputfromuser.toString().trim()
+//     let result= ""
+//     for(let i=num.length-1;i>=0;i--){
+//         result+=num[i];
+//     }console.log(result);
+//     process.exit()
+// });
+// // question-4
+// let arr=[1,2,3,4,5,6,7,8,9,1000,56789];
+// console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
+// //quetsion-5
 
-process.stdout.write("Enter a string : ");
-process.stdin.on("data",function(inputfromuser){
-    let num = inputfromuser.toString().trim();
-    count=0;
-    for(let i=0;i<=num.length-1;i++){
-        if(num.toLowerCase()[i]=="a"||num.toLowerCase()[i]=="o"||num.toLowerCase()[i]=="i"||num.toLowerCase()[i]=="o"||num.toLowerCase()[i]=="u")
-            count+=1;
-        };
-        console.log("NUmebr of vowels = "+count)
-        process.exit();
-    });     
-question-6
-function Fibonaccisequence(num){
-    let a =1,b=2
-    let arr =[]
-    for(let i=0;i<num;i++){
-        arr.push(a);
-        [a,b]=[b,a+b];
-    };
-    console.log(`[${arr}]`)
-};
-Fibonaccisequence(10
+// process.stdout.write("Enter a string : ");
+// process.stdin.on("data",function(inputfromuser){
+//     let num = inputfromuser.toString().trim();
+//     count=0;
+//     for(let i=0;i<=num.length-1;i++){
+//         if(num.toLowerCase()[i]=="a"||num.toLowerCase()[i]=="o"||num.toLowerCase()[i]=="i"||num.toLowerCase()[i]=="o"||num.toLowerCase()[i]=="u")
+//             count+=1;
+//         };
+//         console.log("NUmebr of vowels = "+count)
+//         process.exit();
+//     });     
+// question-6
+// function Fibonaccisequence(num){
+//     let a =1,b=2
+//     let arr =[]
+//     for(let i=0;i<num;i++){
+//         arr.push(a);
+//         [a,b]=[b,a+b];
+//     };
+//     console.log(`[${arr}]`)
+// };
+// Fibonaccisequence(10
 
-)
-queston-7
-function duplicate(arr){
-    for (let i=0;i<=arr.length;i++){
-        for(let j= i+1;j<=arr.length;j++){
-            if(arr[i]===arr[j]){
-                arr.splice(j,1);
-                j--;
-            }
-        }
-    };
-    console.log(arr);
+// )
+// queston-7
+// function duplicate(arr){
+//     for (let i=0;i<=arr.length;i++){
+//         for(let j= i+1;j<=arr.length;j++){
+//             if(arr[i]===arr[j]){
+//                 arr.splice(j,1);
+//                 j--;
+//             }
+//         }
+//     };
+//     console.log(arr);
 
-};
-duplicate([1,2,3,4,5,5,6,7,7,7,7,7,7,7,9])
-//or
-function duplicate(arr){
-    console.log(Math.max(...arr))
-    return [...new Set(arr)];
+// };
+// duplicate([1,2,3,4,5,5,6,7,7,7,7,7,7,7,9])
+// //or
+// function duplicate(arr){
+//     console.log(Math.max(...arr))
+//     return [...new Set(arr)];
     
-};
-console.log(`[${duplicate([1,2,3,4,5,5,6,7,7,7,7,7,7,7,9])}]`)
+// };
+// console.log(`[${duplicate([1,2,3,4,5,5,6,7,7,7,7,7,7,7,9])}]`)
+// //question-8
+// function anagram(str1,str2){
+//     let a = str1.split('').sort().join('');
+//     let b = str2.split('').sort().join('');
+//     if(a===b){
+//         console.log("True")
+//     }else{
+//         console.log("False")
+//     }
+// };
+// anagram("listen","silent")    
+//question-9
